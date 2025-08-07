cb4b5c625f7b9382bb8536040f1b059848f4e58f
AYUSH RAJ <136356003+githubayushraj@users.noreply.github.com>
2025-08-08 03:03:53 +0530

    Update JanusController.java

--- ayush/src/main/java/com/ayush/ayush/controller/JanusController.java
+++ ayush/src/main/java/com/ayush/ayush/controller/JanusController.java
@@ -2,6 +2,8 @@ package com.ayush.ayush.controller;
 
 import com.ayush.ayush.service.JanusService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @RestController
 @RequestMapping("/api/janus")
